aa716c14741a711e29474489b94da72cfce1340b
Stephen Phillips <stephenphillips@lawn-128-61-49-26.lawn.gatech.edu>
2025-04-21 14:45:21 -0400

    Add existing file

---
+++ Assignment_4.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data = {'Age': list(range(0, 101)), 'Fatality Rate': [20/1000000]*18 + [500/1000000]*32 + [6000/1000000]*15 + [90000/1000000]*36}
+dt1 = pd.DataFrame(data)
+dt2 = pd.read_csv("WorldDemographics.csv", index_col=0)
+merged_dt = pd.merge(dt2, dt1, on="Age", how = "left")
+merged_dt["Expected Deaths"] = merged_dt["#Alive"] * merged_dt["Fatality Rate"]
+dt3 = merged_dt.groupby("PopulationID").agg({"#Alive": "sum", "Expected Deaths": "sum"})
+dt3["Percent Died"] = (dt3["Expected Deaths"] / dt3["#Alive"]) * 100
